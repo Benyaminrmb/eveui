ec8300a59ba25aebc4c768cdf1709f9af0479973
Benyamin Rmb <benyaminrmb@gmail.com>
2023-09-15 13:38:54 +0330

    (Fix): created the first tailwind component !.

--- playground/nuxt.config.ts
+++
@@ -1,5 +0,0 @@
-export default defineNuxtConfig({
-  modules: ['../src/module','@nuxtjs/tailwindcss'],
-  myModule: {},
-  devtools: { enabled: true }
-})
--- playground/tailwind.config.js
+++
@@ -1,6 +0,0 @@
-  module.exports = {
-
-  plugins: [
-    require('random-number-generator-rmb'),
-  ],
-}
