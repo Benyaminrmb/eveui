a2ac442032a145fdabb93fb6f4a5821d32c19083
Benyamin Rmb <benyaminrmb@gmail.com>
2023-09-25 07:19:17 +0330

    (Feat): added theme (tests).

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 const plugin = require('tailwindcss/plugin')
+const tailwindColors = require("tailwindcss/colors")
+const colors = require("./theming/index")
 
-const components = require('../dist/styled')
-const utilities = require('../dist/utilities')
-console.log('...EveUI init...')
-const TailwindUiKitPlugin = plugin(
-    function ({ addUtilities, addComponents }) {
-        addUtilities(utilities)
-        addComponents(components)
+
+function mainFunction({ addUtilities, addComponents }) {
+    const components = require('../dist/styled')
+    const utilities = require('../dist/utilities')
+    console.log('...EveUI init...')
+    addUtilities(utilities)
+    addComponents(components)
+}
+module.exports = require("tailwindcss/plugin")(mainFunction, {
+    theme: {
+        extend: {
+            colors: {
+                ...colors,
+                // adding all Tailwind `neutral` shades here so they don't get overridden by daisyUI `neutral` color
+                "aaaaaaaaaa": tailwindColors.red[500],
+                "neutral-50": tailwindColors.neutral[50],
+                "neutral-100": tailwindColors.neutral[100],
+                "neutral-200": tailwindColors.neutral[200],
+                "neutral-300": tailwindColors.neutral[300],
+                "neutral-400": tailwindColors.neutral[400],
+                "neutral-500": tailwindColors.neutral[500],
+                "neutral-600": tailwindColors.neutral[600],
+                "neutral-700": tailwindColors.neutral[700],
+                "neutral-800": tailwindColors.neutral[800],
+                "neutral-900": tailwindColors.neutral[900],
+            },
+        },
     },
-)
-
-module.exports = TailwindUiKitPlugin
+})
---
+++ src/theming/themes.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "theme-1":{
+    "primary": "#09ecf3",
+    "primary-content": "#005355",
+    "secondary": "#966fb3",
+    "accent": "#ffe999",
+    "neutral": "#3b8ac4",
+    "base-100": "#345da7",
+    "info": "#2563eb",
+    "success": "#16a34a",
+    "warning": "#d97706",
+    "error": "#dc2626",
+  }
+}
